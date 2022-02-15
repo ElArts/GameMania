f8cfb8d8598e3226d9200a31d23145a4c23ad9b2
Arthur <arthurdlima99@gmail.com>
2022-02-15 17:55:15 -0300

    considerações finais

--- js/script.js
+++ js/script.js
@@ -8,54 +8,54 @@ $(document).ready(function(){
     })
 })
 
-$(document).ready(function(){
-    $("#carrosel img:eq(0)").addClass("banner-ativo").show()
+// $(document).ready(function(){
+//     $("#carrosel img:eq(0)").addClass("banner-ativo").show()
+// })
 
+// setInterval(slide,5000)
+// function slide(){
+//     if($(".banner-ativo").next().length){
+//         $(".banner-ativo").removeClass("banner-ativo").hide().next().addClass("banner-ativo").show()
+//     }else{
+//         $(".banner-ativo").removeClass().hide()
+//         $("#carrosel img:eq(0)").addClass("banner-ativo").show()
+//     }
+// }
 
-setInterval(slide,5000)
-function slide(){
-    if($(".banner-ativo").next().length){
-        $(".banner-ativo").removeClass("banner-ativo").hide().next().addClass("banner-ativo").show()
-    }else{
-        $(".banner-ativo").removeClass().hide()
-        $("#carrosel img:eq(0)").addClass("banner-ativo").show()
-    }
-}
-})
-let listaNovositens = [
-    {
-        descricao: "Placa GTX 1050 Ti DDR5, 4GB de RAM",
-    },
-    {
-        descricao: "Placa GTX 1050 Ti DDR5, 4GB de RAM",
-    },
-    {
-        descricao: "Placa GTX 1050 Ti DDR5, 4GB de RAM",
-    },
-    {
-        descricao: "Placa GTX 1050 Ti DDR5, 4GB de RAM",
-    },
-    {
-        descricao: "Placa GTX 1050 Ti DDR5, 4GB de RAM",
-    },
-    {
-        descricao: "Placa GTX 1050 Ti DDR5, 4GB de RAM",
-    },
-]
+// let listaNovositens = [
+//     {
+//         descricao: "Placa GTX 1050 Ti DDR5, 4GB de RAM",
+//     },
+//     {
+//         descricao: "Placa GTX 1050 Ti DDR5, 4GB de RAM",
+//     },
+//     {
+//         descricao: "Placa GTX 1050 Ti DDR5, 4GB de RAM",
+//     },
+//     {
+//         descricao: "Placa GTX 1050 Ti DDR5, 4GB de RAM",
+//     },
+//     {
+//         descricao: "Placa GTX 1050 Ti DDR5, 4GB de RAM",
+//     },
+//     {
+//         descricao: "Placa GTX 1050 Ti DDR5, 4GB de RAM",
+//     },
+// ]
 
-function renderizarItens(){
-    let espaco = document.getElementById("id-itens")
+// function renderizarItens(){
+//     let espaco = document.getElementById("id-itens")
 
-    let template ="";
+//     let template ="";
 
-    for (let index = 0; index < listaNovositens.length; index++) {
-        const itens = listaNovositens[index];
+//     for (let index = 0; index < listaNovositens.length; index++) {
+//         const itens = listaNovositens[index];
         
-        template += `<div class="carditens">
-        <img src="img/gtx.jpg" alt="imagem gtx 1050ti">
-        <h3>${itens.descricao}</h3>
-    </div>`
-    }
+//         template += `<div class="carditens">
+//         <img src="img/gtx.jpg" alt="imagem gtx 1050ti">
+//         <h3>${itens.descricao}</h3>
+//     </div>`
+//     }
 
-    espaco.innerHTML = template;
-}
+//     espaco.innerHTML = template;
+// }
